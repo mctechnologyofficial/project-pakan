08a3f1687daf1fd8996d6005a72f17b4fada525a
DewanataPandu <dewanatapandu145@gmail.com>
2023-02-08 12:49:03 +0700

    layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,49 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<head>
+    <meta charset="utf-8">
+    <title>Milky - Dairy Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Favicon -->
+    @include('Components.css')
+</head>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" role="status" style="width: 3rem; height: 3rem;"></div>
     </div>
-</div>
-@endsection
+    <!-- Spinner End -->
+
+
+    <!-- Topbar Start -->
+    @include('Components.topbar')
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    @include('Components.navbar')
+    <!-- Navbar End -->
+
+
+    @yield('content')
+
+
+    <!-- Footer Start -->
+    @include('Components.footer')
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    @include('Components.js')
+</body>
+
+</html>
