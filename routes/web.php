272d47c687cd35bec376629ae208d701300c25aa
DewanataPandu <dewanatapandu145@gmail.com>
2023-02-08 13:53:56 +0700

    layout login

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/login', function () {
-    return view('auth.login');
+    return view('auth.credential');
 });
 
 // force logout routes, temporary for debugging
